0d2a640c2868d8023e50f8d905e560847570d3e3
lujhu <michaelmcalpine@gmail.com>
2014-06-18 21:26:58 -0400

    using try and except

--- tryandexcept.py
+++ tryandexcept.py
@@ -1,1 +1,18 @@
-this is my try and except excercise
+hrs = raw_input('Enter Hours: ')
+try:
+    h = float(hrs)
+except:
+    print "Please enter a numeric value."	
+    exit()
+rte = raw_input('Enter Rate: ') 
+try:
+    r = float(rte)
+except:
+    print "Please enter a numeric value."	
+    exit()
+if h > 40:
+    ot = (h-40)*1.5
+    pay = ot * r + 40 * r
+else:
+    pay = h	* r
+print pay
